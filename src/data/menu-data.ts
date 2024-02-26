9de543452c06870493587f9f126981471dc38cdd
Deepak Rawat <rwtroshan28@gmail.com>
2024-02-26 11:22:02 +0530

    link update

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -88,7 +88,7 @@ const menu_data:IMenu[] = [
     title:'Company ',
     dropdown:true,
     dropdown_menus:[
-          {link:'/coming-soon',title:'Our Story'},
+          {link:'/our-story',title:'Our Story'},
           {link:'/vision',title:'Vision'},
           {link:'/mission',title:'Mission'},
           {link:'/awards',title:'Awards'},
